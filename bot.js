757d60d2cedbce4562fa8f8b010e1010d998c7de
axrynz <laura.alcon@iesdonana.org>
2023-01-05 20:38:50 +0100

    event & command handling + dotenv -> config.json

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const fs = require('node:fs');
 const path = require('node:path');
 // discord.js classes
-require("dotenv").config(); //to start process from .env file
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// load commands
+// COMMAND HANDLING - load commands
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands'); // builds a path to commands directory
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -18,28 +18,18 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-// message when client is ready. c parameter keeps it separate from client
-client.once(Events.ClientReady, c => { // node bot.js
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-})
+// EVENT HANDLING - retrieving event files
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-// command handling: receiving command interactions
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
+}
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
-client.login(process.env.TOKEN);
+client.login(token);
