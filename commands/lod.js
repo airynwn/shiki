71917edc439a680f0d31733f56523653e8fbd9e2
airynwn <laura.alcon@iesdonana.org>
2024-08-26 19:47:58 +0200

    code refactoring

--- commands/lod.js
+++ commands/lod.js
@@ -2,6 +2,26 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 const channels = ["7", "1", "1", "2", "3", "2, 3 y 6", "4, 5 y 7", "4, 5 y 6"];
 
+// !! TODO ARREGLAR QUÉ PASA SI LOS MINUTOS ACTUALES SON :00
+function untilNext(min, hour) {   
+    const nextLod = channels.findIndex((_, row) => row * 3 > hour) * 3;
+    const hoursUntilNext = nextLod - hour - 1;
+    const minsUntilNext = 60 - min;
+    return `Quedan ${hoursUntilNext} horas y ${minsUntilNext} minutos para el próximo LoD.`;
+}
+
+function currentLod(min, hour) {
+    const hoursPassed = hour % 3;
+    const minsUntilNext = 60 - min;
+
+    if (hoursPassed >= 2) {
+      return "No hay LoD ahora mismo.";
+    } else if (hoursPassed == 1) { 
+      return `Quedan ${minsUntilNext} minutos para que cierre LoD.`;
+    } else {
+      return `Quedan ${minsUntilNext} minutos para que salga coco.`;
+    }
+  }
 
 // const channels = ["7", "1", "1", "2", "3", "2, 3 y 6", "4, 5 y 7", "4, 5 y 6"];
 // let time = new Date();
@@ -58,12 +78,8 @@ function createLodEmbed() {
     let min = time.getMinutes();
     let now = hour + ":" + (min > 10 ? min : "0" + min);
     let row = Math.floor(hour / 3);
-    let nextLod = channels.findIndex((_, row) => row * 3 > hour) * 3;
     let lastLod = 3 * row;
-    let hoursUntilNext = nextLod - hour - 1;
     let hoursPassed = hour - lastLod;
-    let minsUntilNext = 60 - min;
-    // !! TODO ARREGLAR QUE PASA SI LOS MINUTOS ACTUALES SON :00
 
     function getChannels(time) {
         if (time == "current") {
@@ -73,28 +89,14 @@ function createLodEmbed() {
         }
     }
 
-    function untilNext() {
-        return `Quedan ${hoursUntilNext} horas y ${minsUntilNext} minutos para el próximo LoD a las ${nextLod}:00 en ${getChannels("next")}.`;
-    }
-
-    function currentLod() {
-        if (hoursPassed >= 2) {
-            return "No hay LoD ahora mismo."
-        } else if (hoursPassed == 1)  {
-            return `Quedan ${minsUntilNext} minutos para que cierre LoD.`
-        } else {
-            return `Quedan ${minsUntilNext} minutos para que salga coco.`;
-        }
-    }
-
     return new EmbedBuilder()
 	.setColor(14548736)
 	.setTitle('Horarios de LoD')
 	.setDescription(`Son las ${now}.`)
 	.addFields(
 		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Actual', value: hoursPassed < 2 ? `Hay LoD ahora mismo en ${getChannels("current")}. ${currentLod()}` : `(El próximo LoD es a las ${nextLod}:00 en ${getChannels("next")}. ${untilNext()}`, inline: true },
-		{ name: 'Próximo', value: `${untilNext()}`, inline: true },
+		{ name: 'Actual', value: hoursPassed < 2 ? `Hay LoD ahora mismo en ${getChannels("current")}. ${currentLod(min, hour)}` : `(El próximo LoD es a las ${nextLod}:00 en ${getChannels("next")}. ${untilNext(min, hour)}`, inline: true },
+		{ name: 'Próximo', value: `${untilNext(min, hour)}`, inline: true },
 	)
 	.setImage('https://cdn-longterm.mee6.xyz/plugins/embeds/images/628543142819528714/3e2963015e4118ab82c55bf85df0c4e8dfb34ef6737d8fa87c3df78df9a090ef.png')
 }
