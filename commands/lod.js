dd273b22ae9dd1557f11f175b654b43d4ca89cda
airynwn <laura.alcon@iesdonana.org>
2024-08-26 20:39:07 +0200

    code refactoring 2

--- commands/lod.js
+++ commands/lod.js
@@ -1,27 +1,3 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-
-const channels = ["7", "1", "1", "2", "3", "2, 3 y 6", "4, 5 y 7", "4, 5 y 6"];
-
-// !! TODO ARREGLAR QUÉ PASA SI LOS MINUTOS ACTUALES SON :00
-function untilNext(min, hour) {   
-    const nextLod = channels.findIndex((_, row) => row * 3 > hour) * 3;
-    const hoursUntilNext = nextLod - hour - 1;
-    const minsUntilNext = 60 - min;
-    return `Quedan ${hoursUntilNext} horas y ${minsUntilNext} minutos para el próximo LoD.`;
-}
-
-function currentLod(min, hour) {
-    const hoursPassed = hour % 3;
-    const minsUntilNext = 60 - min;
-
-    if (hoursPassed >= 2) {
-      return "No hay LoD ahora mismo.";
-    } else if (hoursPassed == 1) { 
-      return `Quedan ${minsUntilNext} minutos para que cierre LoD.`;
-    } else {
-      return `Quedan ${minsUntilNext} minutos para que salga coco.`;
-    }
-  }
 
 // const channels = ["7", "1", "1", "2", "3", "2, 3 y 6", "4, 5 y 7", "4, 5 y 6"];
 // let time = new Date();
@@ -69,6 +45,36 @@ function currentLod(min, hour) {
 // 		{ name: 'Próximo', value: `${untilNext()}`, inline: true },
 // 	)
 // 	.setImage('https://cdn-longterm.mee6.xyz/plugins/embeds/images/628543142819528714/3e2963015e4118ab82c55bf85df0c4e8dfb34ef6737d8fa87c3df78df9a090ef.png')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const channels = ["7", "1", "1", "2", "3", "2, 3 y 6", "4, 5 y 7", "4, 5 y 6"];
+
+// !! TODO ARREGLAR QUÉ PASA SI LOS MINUTOS ACTUALES SON :00
+function untilNext(min, hour) {   
+    let nextLod = channels.findIndex((_, row) => row * 3 > hour) * 3;
+    let hoursUntilNext = nextLod - hour - 1;
+    let minsUntilNext = 60 - min;
+
+    if (minsUntilNext == 60) {
+        minsUntilNext = 0;
+        hoursUntilNext = hoursUntilNext + 1;
+    }
+
+    return `Quedan ${hoursUntilNext} hora${hoursUntilNext == 1 ? "" : "s"} y ${minsUntilNext} minuto${minsUntilNext == 1 ? "" : "s"} para el próximo LoD.`;
+}
+
+function currentLod(min, hour) {
+    const hoursPassed = hour % 3;
+    const minsUntilNext = 60 - min;
+
+    if (hoursPassed >= 2) {
+      return "No hay LoD ahora mismo.";
+    } else if (hoursPassed == 1) { 
+      return `Quedan ${minsUntilNext} minutos para que cierre LoD.`;
+    } else {
+      return `Quedan ${minsUntilNext} minutos para que salga coco.`;
+    }
+  }
 
 // * ----------------------------- *
 
@@ -78,8 +84,8 @@ function createLodEmbed() {
     let min = time.getMinutes();
     let now = hour + ":" + (min > 10 ? min : "0" + min);
     let row = Math.floor(hour / 3);
-    let lastLod = 3 * row;
-    let hoursPassed = hour - lastLod;
+    let nextLod = channels.findIndex((_, row) => row * 3 > hour) * 3; 
+    let hoursPassed = hour % 3;
 
     function getChannels(time) {
         if (time == "current") {
@@ -95,7 +101,7 @@ function createLodEmbed() {
 	.setDescription(`Son las ${now}.`)
 	.addFields(
 		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Actual', value: hoursPassed < 2 ? `Hay LoD ahora mismo en ${getChannels("current")}. ${currentLod(min, hour)}` : `(El próximo LoD es a las ${nextLod}:00 en ${getChannels("next")}. ${untilNext(min, hour)}`, inline: true },
+		{ name: 'Actual', value: hoursPassed < 2 ? `Hay LoD ahora mismo en ${getChannels("current")}. ${currentLod(min, hour)}` : `El próximo LoD es a las ${nextLod}:00 en ${getChannels("next")}.`, inline: true },
 		{ name: 'Próximo', value: `${untilNext(min, hour)}`, inline: true },
 	)
 	.setImage('https://cdn-longterm.mee6.xyz/plugins/embeds/images/628543142819528714/3e2963015e4118ab82c55bf85df0c4e8dfb34ef6737d8fa87c3df78df9a090ef.png')
@@ -111,5 +117,3 @@ module.exports = {
         await interaction.channel.send({embeds: [createLodEmbed()]});
 	},
 };
-
-// TODO son las X horas (españa), falta X para lod || esta lod activo?->falta X para coco->canal X
