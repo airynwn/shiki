e9b76e5125715327492be618bcab4f2db5a9d886
airynwn <laura.alcon@iesdonana.org>
2024-09-21 02:21:35 +0200

    upgrade data

---
+++ data.js
@@ -0,0 +1,185 @@
+module.exports = {
+    upgrades: {
+        1: {
+            gold: 200000,
+            full_moon: 1,
+            angels_feather: 3,
+            soul: 2,
+            success: 0.80,
+            destruction: 0,
+            failure: 0.20
+        },
+        2: {
+            gold: 200000,
+            full_moon: 3,
+            angels_feather: 5,
+            soul: 4,
+            success: 0.75,
+            destruction: 0,
+            failure: 0.25
+            
+        },
+        3: {
+            gold: 200000,
+            full_moon: 5,
+            angels_feather: 8,
+            soul: 6,
+            success: 0.70,
+            destruction: 0.05,
+            failure: 0.25
+        },
+        4: {
+            gold: 200000,
+            full_moon: 7,
+            angels_feather: 10,
+            soul: 8,
+            success: 0.60,
+            destruction: 0.10,
+            failure: 0.30
+        },
+        5: {
+            gold: 200000,
+            full_moon: 10,
+            angels_feather: 15,
+            soul: 10,
+            success: 0.50,
+            destruction: 0.15,
+            failure: 0.35
+            },
+        6: {
+            gold: 500000,
+            full_moon: 12,
+            angels_feather: 20,
+            soul: 1,
+            success: 0.40,
+            destruction: 0.20,
+            failure: 0.40
+        },
+        7: {
+            gold: 500000,
+            full_moon: 14,
+            angels_feather: 25,
+            soul: 2,
+            success: 0.35,
+            destruction: 0.25,
+            failure: 0.40
+        },
+        8: {
+            gold: 500000,
+            full_moon: 16,
+            angels_feather: 30,
+            soul: 3,
+            success: 0.30,
+            destruction: 0.30,
+            failure: 0.40
+        },
+        9: {
+            gold: 500000,
+            full_moon: 18,
+            angels_feather: 35,
+            soul: 4,
+            success: 0.25,
+            destruction: 0.35,
+            failure: 0.40
+        },
+        10: {
+            gold: 500000,
+            full_moon: 20,
+            angels_feather: 40,
+            soul: 5,
+            success: 0.20,
+            destruction: 0.40,
+            failure: 0.40
+        },
+        11: {
+            gold: 1000000,
+            full_moon: 22,
+            angels_feather: 45,
+            soul: 1,
+            success: 0.10,
+            destruction: 0.45,
+            failure: 0.45
+        },
+        12: {
+            gold: 1000000,
+            full_moon: 24,
+            angels_feather: 50,
+            soul: 2,
+            success: 0.07,
+            destruction: 0.50,
+            failure: 0.43
+        },
+        13: {
+            gold: 1000000,
+            full_moon: 26,
+            angels_feather: 55,
+            soul: 3,
+            success: 0.05,
+            destruction: 0.55,
+            failure: 0.40
+        },
+        14: {
+            gold: 1000000,
+            full_moon: 28,
+            angels_feather: 60,
+            soul: 4,
+            success: 0.03,
+            destruction: 0.60,
+            failure: 0.37
+        },
+        15: {
+            gold: 1000000,
+            full_moon: 30,
+            angels_feather: 70,
+            soul: 5,
+            success: 0.15,
+            destruction: 0.70,
+            failure: 0.285
+        },
+        16: {
+            gold: 1250000,
+            full_moon: 32,
+            angels_feather: 80,
+            soul: 2,
+            success: 0.12,
+            destruction: 0.70,
+            failure: 0.288
+        },
+        17: {
+            gold: 1500000,
+            full_moon: 34,
+            angels_feather: 90,
+            soul: 4,
+            success: 0.1,
+            destruction: 0.70,
+            failure: 0.29
+        },
+        18: {
+            gold: 1750000,
+            full_moon: 36,
+            angels_feather: 100,
+            soul: 6,
+            success: 0.08,
+            destruction: 0.75,
+            failure: 0.242
+        },
+        19: {
+            gold: 2000000,
+            full_moon: 38,
+            angels_feather: 110,
+            soul: 8,
+            success: 0.06,
+            destruction: 0.80,
+            failure: 0.194
+        },
+        20: {
+            gold: 2250000,
+            full_moon: 40,
+            angels_feather: 120,
+            soul: 10,
+            success: 0.04,
+            destruction: 0.80,
+            failure: 0.196
+        }
+    }
+}
